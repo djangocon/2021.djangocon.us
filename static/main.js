4940a8d7037240303c48bb62e665666070f2a931
Michael Trythall <m@mtrythall.com>
2019-03-12 20:21:33 -0400

    Adds skip links for schedule

--- static/main.js
+++ static/main.js
@@ -23566,7 +23566,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63614" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50189" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
