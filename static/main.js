0c9e1015d517bca5a97f2b196455f70eff9165a7
Michael Trythall <m@mtrythall.com>
2019-03-27 21:25:05 -0400

    Updates to recap page

--- static/main.js
+++ static/main.js
@@ -23562,7 +23562,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65503" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51313" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
