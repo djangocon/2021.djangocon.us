a664eafa7142330db18a8dec9139444fe3f75ccd
Michael Trythall <m@mtrythall.com>
2020-03-11 12:34:57 -0400

    Simplify navigation. Add pre-footer curve and photo

--- _src/main.js
+++ _src/main.js
@@ -2,18 +2,8 @@
 import jQuery from 'jquery';
 import 'what-input';
 import 'foundation-sites';
-import Headroom from 'headroom.js';
 
 window.$ = window.jQuery = jQuery;
-
-// Setup header
-const headroomElem = document.querySelector(".headroom");
-// construct an instance of Headroom, passing the element
-const headroom  = new Headroom(headroomElem);
-// initialise
-headroom.init({
-  offset : 200,
-});
 
 // init Foundation
 $(document).foundation();
@@ -23,11 +13,3 @@ $("[data-off-canvas-wrapper]")
   .bind("opened.zf.offcanvas closed.zf.offcanvas", function(e) {
     $(".hamburger").toggleClass("is-active");
 });
-
-/*
-const hour = (new Date()).getHours();
-// If it's after 6pm or before 7am, set the Sunset effect.
-if (hour > 18 || hour < 7) {
-  $("body").addClass('sunset');
-}
-*/
